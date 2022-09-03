693a7c0d209f8e166f72e3c09f115387b9565666
MexL <madiajijah7@gmail.com>
2022-09-03 15:15:56 +0800

    nextjs rest prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -48,22 +48,23 @@ const userData: Prisma.UserCreateInput[] = [
   },
 ]
 
-export async function main() {
-  try {
-    console.log(`Start seeding ...`)
-    for (const u of userData) {
-      const user = await prisma.user.create({
-        data: u,
-      })
-      console.log(`Created user with id: ${user.id}`)
-    }
-    console.log(`Seeding finished.`)
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
-  } finally {
-    await prisma.$disconnect()
+async function main() {
+  console.log(`Start seeding ...`)
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    })
+    console.log(`Created user with id: ${user.id}`)
   }
+  console.log(`Seeding finished.`)
 }
 
 main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
